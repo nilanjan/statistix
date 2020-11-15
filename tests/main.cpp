2eaf9fe010a392b60e1979d71e4c147549243e18
nilanjan <nilanjan.goswami@gmail.com>
2020-11-14 19:11:46 -0800

    catch2 added.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,7 +3,24 @@
 //
 
 #define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include "TestCase.hpp"
 
 
+unsigned int Factorial( unsigned int number ) {
+  return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial1]" ) {
+  REQUIRE( Factorial(1) == 1 );
+  REQUIRE( Factorial(2) == 2 );
+  REQUIRE( Factorial(3) == 6 );
+  REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE( "Computed", "[factorial2]" ) {
+  REQUIRE( Factorial(1) == 1 );
+  REQUIRE( Factorial(2) == 2 );
+  REQUIRE( Factorial(3) == 6 );
+  REQUIRE( Factorial(10) == 3628800 );
+}
