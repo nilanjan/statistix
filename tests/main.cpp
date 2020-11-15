44e280e733da187f4b5cf64e9f0d1b9e5c6d7e21
nilanjan <nilanjan.goswami@gmail.com>
2020-11-14 21:41:08 -0800

    Another test added.

--- tests/main.cpp
+++ tests/main.cpp
@@ -11,14 +11,21 @@ unsigned int Factorial( unsigned int number ) {
   return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial1]" ) {
+TEST_CASE( "TC1", "[factorial1]" ) {
   REQUIRE( Factorial(1) == 1 );
   REQUIRE( Factorial(2) == 2 );
   REQUIRE( Factorial(3) == 6 );
   REQUIRE( Factorial(10) == 3628800 );
 }
 
-TEST_CASE( "Computed", "[factorial2]" ) {
+TEST_CASE( "TC2", "[factorial2]" ) {
+  REQUIRE( Factorial(1) == 1 );
+  REQUIRE( Factorial(2) == 2 );
+  REQUIRE( Factorial(3) == 6 );
+  REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE( "TC3", "[factorial3]" ) {
   REQUIRE( Factorial(1) == 1 );
   REQUIRE( Factorial(2) == 2 );
   REQUIRE( Factorial(3) == 6 );
